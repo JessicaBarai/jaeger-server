7999ba106b170238d088e105e5c03a773169c46d
JessicaBarai <81428391+JessicaBarai@users.noreply.github.com>
2021-07-01 17:15:19 +0200

    Update JaegerServerApplication.java

--- src/main/java/com/techprimers/jaegerserver/JaegerServerApplication.java
+++ src/main/java/com/techprimers/jaegerserver/JaegerServerApplication.java
@@ -18,6 +18,7 @@ public class JaegerServerApplication {
 
 
 	public static void main(String[] args) {
+		System.out.print("STARTING!");
 		SpringApplication.run(JaegerServerApplication.class, args);
 	}
 
