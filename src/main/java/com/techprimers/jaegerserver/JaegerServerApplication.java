d9806ba8ea106dfb65e9e45862e3373b4da89c23
JessicaBarai <81428391+JessicaBarai@users.noreply.github.com>
2021-07-05 10:55:08 +0200

    Update JaegerServerApplication.java

--- src/main/java/com/techprimers/jaegerserver/JaegerServerApplication.java
+++ src/main/java/com/techprimers/jaegerserver/JaegerServerApplication.java
@@ -1,24 +1,27 @@
 package com.techprimers.jaegerserver;
 
 import org.springframework.boot.SpringApplication;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class JaegerServerApplication {
 	
-	@GetMapping("/")
-	public String welcome() {
-		System.out.print("change enabled");
-		return "Hello from server!";
+	@RequestMapping("/")
+	public String welcome() throws JSONException {
+		JSONObject jsonObject= new JSONObject();
+		jsonObject.put("Message", "Hello from server");
+		return jsonObject.toString();
 		
 	}
 
 
 	public static void main(String[] args) {
-		System.out.print("STARTING WITH PROPERTIES CHANGED1.2!");
+		System.out.print("STARTING WITH PROPERTIES CHANGED!");
 		SpringApplication.run(JaegerServerApplication.class, args);
 	}
 
