c9d5f3c3f4b096c8fd2805a67614ffd3a6f224cb
JessicaBarai <81428391+JessicaBarai@users.noreply.github.com>
2021-07-06 21:18:34 +0200

    Update JaegerConfig.java

--- src/main/java/com/techprimers/jaegerserver/config/JaegerConfig.java
+++ src/main/java/com/techprimers/jaegerserver/config/JaegerConfig.java
@@ -14,13 +14,21 @@ public class JaegerConfig {
     return WebClient.create();
   }
 
-  @Bean
-  public JaegerTracer jaegerTracer() {
+ @Bean
+	public JaegerTracer jaegerTracer() {
+			  SamplerConfiguration samplerConfig = SamplerConfiguration.fromEnv().withType(ConstSampler.TYPE).withParam(1);
+			  ReporterConfiguration reporterConfig = ReporterConfiguration.fromEnv().withLogSpans(true).withSender(
+			      SenderConfiguration.fromEnv().withAgentHost("localhost").withAgentPort(6831));
+			  io.jaegertracing.Configuration config =  new io.jaegertracing.Configuration("jaeger-server").withSampler(samplerConfig).withReporter(reporterConfig);
+			  return config.getTracer();
+			
+		
+	//return new io.jaegertracing.internal.JaegerTracer.Builder("jaeger-server").build();
 
-    return new io.jaegertracing.Configuration("jaeger-server")
-        .withSampler(new io.jaegertracing.Configuration.SamplerConfiguration().withType(ConstSampler.TYPE)
-        .withParam(1))
-        .withReporter(new io.jaegertracing.Configuration.ReporterConfiguration().withLogSpans(true))
-        .getTracer();
-  }
+	/*return new io.jaegertracing.Configuration("jaeger-server")
+				.withSampler(new io.jaegertracing.Configuration.SamplerConfiguration().withType(ConstSampler.TYPE)
+						.withParam(1))
+				.withReporter(new io.jaegertracing.Configuration.ReporterConfiguration().withLogSpans(true))
+				.getTracer();*/
+	}
 }
