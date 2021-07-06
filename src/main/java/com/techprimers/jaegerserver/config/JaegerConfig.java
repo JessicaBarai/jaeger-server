b4f6eac32a931a5b145489dc4ecc80f2e40117ad
JessicaBarai <81428391+JessicaBarai@users.noreply.github.com>
2021-07-06 21:25:37 +0200

    Update JaegerConfig.java

--- src/main/java/com/techprimers/jaegerserver/config/JaegerConfig.java
+++ src/main/java/com/techprimers/jaegerserver/config/JaegerConfig.java
@@ -1,20 +1,29 @@
 package com.techprimers.jaegerserver.config;
 
+import io.jaegertracing.Configuration.ReporterConfiguration;
+import io.jaegertracing.Configuration.SamplerConfiguration;
+import io.jaegertracing.Configuration.SenderConfiguration;
 import io.jaegertracing.internal.JaegerTracer;
 import io.jaegertracing.internal.samplers.ConstSampler;
+
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
+//this is a configuration where we going to inject two beans
 @Configuration
 public class JaegerConfig {
+//WebClient is going to be used to connect client to server, , reactive client to perform HTTP requests, exposing a fluent, reactive API over underlying HTTP client libraries such as Reactor Netty.
+	@Bean
+	public WebClient webClient() {
+		return WebClient.create();
+	}
 
-  @Bean
-  public WebClient webClient() {
-    return WebClient.create();
-  }
-
- @Bean
+	//this is wrong, it should listen to properties file or we should use enviroment variable. Look it up!
+	//with this code it is running by deafult at localhost. Make sure it listenes to enviroment variable 
+	// ask maciej tomrrow!
+	@Bean
 	public JaegerTracer jaegerTracer() {
 			  SamplerConfiguration samplerConfig = SamplerConfiguration.fromEnv().withType(ConstSampler.TYPE).withParam(1);
 			  ReporterConfiguration reporterConfig = ReporterConfiguration.fromEnv().withLogSpans(true).withSender(
@@ -31,4 +40,5 @@ public class JaegerConfig {
 				.withReporter(new io.jaegertracing.Configuration.ReporterConfiguration().withLogSpans(true))
 				.getTracer();*/
 	}
+
 }
